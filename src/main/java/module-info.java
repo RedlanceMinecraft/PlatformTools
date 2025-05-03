3391fc7df3c7ee05d5f645906822720df3807de5
dima_dencep <dima_dencep@redlance.org>
2025-05-03 18:38:30 +0700

    Fix jfa
    https://github.com/zacharee/jfa/pull/1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module platformtools {
     exports org.redlance.platformtools;
     exports org.redlance.platformtools.impl;
 
+    requires jfa;
     requires com.sun.jna;
     requires com.sun.jna.platform;
 
     requires java.desktop;
-    requires jfa.main.SNAPSHOT;
 }
